c56e7b3e70199103097536228ab8824e5efd84e9
“neilburd” <neilburd@gmail.com>
2016-12-20 06:15:49 -0500

    view houses and students fixed for now

--- db/migrate/20131030184440_create_students.rb
+++ db/migrate/20131030184440_create_students.rb
@@ -2,8 +2,8 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :name, null: false
-      t.string :img_url
-      t.references :house, null: false, index:true
+      t.string :image_url
+      t.references :house, null: false, index: true
 
       t.timestamps
     end
---
+++ db/migrate/20161219215402_fix_shit_again.rb
@@ -0,0 +1,4 @@
+class FixShitAgain < ActiveRecord::Migration[5.0]
+  def change
+  end  #Change the img_url to image_
+end
